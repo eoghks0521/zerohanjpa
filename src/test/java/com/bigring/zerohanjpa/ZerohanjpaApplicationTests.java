169a32aa0b71c0d6b23cc072c63b1a9413e802e7
권대환 <bigring@wemakeprice.com>
2021-04-11 15:20:38 +0900

    initialize project

---
+++ src/test/java/com/bigring/zerohanjpa/ZerohanjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigring.zerohanjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerohanjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
